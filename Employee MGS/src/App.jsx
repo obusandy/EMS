739c68d0001cd7bc9b183283137c25cb8ddc7e69
obusandy <sandraobunga29@gmail.com>
2024-04-26 16:59:45 +0300

    Routes

--- Employee MGS/src/App.jsx
+++ Employee MGS/src/App.jsx
@@ -3,6 +3,10 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Login from './components/Login';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Dashboard from './components/Dashboard';
+import Home from './components/Home';
+import Employee from './components/Employee';
+import Category from './components/Category';
+import Profile from './components/Profile';
 
 function App() {
  
@@ -11,7 +15,12 @@ function App() {
    <BrowserRouter>
    <Routes>
     <Route path='/adminlogin' element={<Login />}></Route>
-    <Route path='/dashboard' element={<Dashboard />}></Route>
+    <Route path='/dashboard' element={<Dashboard />}>
+      <Route path='' element={<Home />}></Route>
+      <Route path='/employee' element={<Employee />}></Route>
+      <Route path='/category' element={<Category />}></Route>
+      <Route path='/profile' element={<Profile />}></Route>
+    </Route>
    </Routes>
    </BrowserRouter>
   )
--- Employee MGS/src/components/Dashboard.jsx
+++ Employee MGS/src/components/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
@@ -9,7 +9,8 @@ const Dashboard = () => {
       <div className="row flex-nowrap">
         <div className='col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark'>
           <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-              <Link to="/dashboard" className="d-flex align-items-center pb-3 mb-md-1 mt-md-3 me-md-auto text-white text-decoration-none"><span className='fs-5 fw-bolder d-none d-sm-inline'>E.M.S</span></Link>
+              <Link to="/dashboard" className="dashboard-logo d-flex align-items-center pb-3 mb-md-1 mt-md-3 me-md-auto text-white text-decoration-none">
+                <span className='fs-5 fw-bolder d-none d-sm-inline'>E.M.S</span></Link>
               <ul className='nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start' id='menu'>
                 <li className='w-100'>
                   <Link to="/dashboard" className='nav-link text-white px-0 align-middle'>
@@ -35,13 +36,19 @@ const Dashboard = () => {
                     </Link>
                 </li>
                 <li className='w-100'>
-                  <Link  className='nav-link text-white px-0 align-middle'>
+                  <Link to="/dashboard/logout" className='nav-link text-white px-0 align-middle'>
                   <i className='fs-4 bi-power ms-2'></i>
                     <span className='ms-2 d-none d-sm-inline'>Log-out</span>
                     </Link>
                 </li>
               </ul>
           </div>
+        </div>
+        <div className='col p-0 m-0'>
+            <div className='p-2 d-flex justify-content-center shadow'>
+<h2>Employee Management System</h2>
+            </div>
+            <Outlet />
         </div>
       </div>
     </div>
